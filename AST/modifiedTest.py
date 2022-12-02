e42821aa569b30d01bb4eb4c2cc416a40b25c11b
Sienna <62346172+Drafee@users.noreply.github.com>
2022-12-02 23:01:51 +0800

    Add files via upload
    cleaned AST

---
+++ AST/modifiedTest.py
@@ -0,0 +1,182 @@
+import ast
+import astpretty
+
+DEFAULT_INDENT = "  "
+
+class TreeNode:
+    def __init__(self, content): 
+        self.content = content
+        self.children = []
+    def addChild(self, t):
+        self.children.append(t)
+
+
+class CreateTuple():
+    def __init__(self, name):
+        self.text_file = open(name, "r")
+        #read whole file to a string
+        self.data = self.text_file.read()
+        self.text_file.close()
+        self.result = astpretty.pformat(ast.parse(self.data).body[0],indent = DEFAULT_INDENT,show_offsets=False)
+        # print(self.result)
+        self.all_tuples = []
+        self.parsed = self.result.split("\n")
+
+    def generate_tuples(self, parsed = [], withIndentNum = 0, parent = ""):
+        if (parsed == []):
+            parsed = self.result.split("\n")
+        next_round_parsed = []
+        currentNode = None
+        for item in parsed:
+            if (self.start(item) == DEFAULT_INDENT * withIndentNum):
+                if (next_round_parsed == []):
+                    if (item.strip().startswith("value=Constant") and (parent.content == "ex")):
+                        continue
+                    if (deal_with_item(item.strip())):
+                        currentNode = TreeNode(deal_with_item(item.strip()))
+                        parent.addChild(currentNode)
+                else:
+                    self.generate_tuples(next_round_parsed, withIndentNum + 1, currentNode)
+                    next_round_parsed = []
+                    if (item.strip().startswith("value=Constant") and (parent.content == "ex")):
+                        continue
+                    if (deal_with_item(item.strip())):
+                        currentNode = TreeNode(deal_with_item(item.strip()))
+                        parent.addChild(currentNode)
+            else:
+                # print(next_round_parsed)
+                next_round_parsed.append(item)
+            
+
+    def start(self, string):
+        output = ""
+        for i in range(len(string)):
+            if (string[i] == " "):
+                output += " "
+            else :
+                return output
+
+def deal_with_item(item):
+
+    # clean data
+    if item in ["],", "),", ")", "ctx=Store(),", "ctx=Load(),", "returns=None,"]:
+        return
+    elif item.endswith("=[],"):
+        return
+    elif item.endswith("=None,"):
+        return
+    # simplify expression
+    elif item == "ClassDef(":
+        return "c"
+    elif item == "body=[":
+        return "b"
+    elif item == "FunctionDef(":
+        return "f"
+    elif item == "Expr(":
+        return "ex"
+    elif item == "args=arguments(":
+        return "aar"
+    elif item == "Assign(":
+        return "A"
+    elif item == "args=[":
+        return "args"
+    elif item == "Attribute(":
+        return "At"
+    elif item == "targets=[":
+        return "t"
+    elif item == "Tuple(":
+        return "tu"
+    elif item == "AugAssign(":
+        return "aua"
+    elif item.startswith("name="):
+        return "n"
+    elif "attr=" in item:
+        return item.replace("attr=", "").replace("'","")
+    elif item.startswith("value=Constant"):
+        return valueConstant(item)
+
+    elif item.startswith("arg(arg=") or item.startswith("args=[arg(arg"):
+        return arg(item)
+
+    elif item.startswith("value=Name"):
+        return value(item)
+    elif item.startswith("left=Name"):
+        return leftName(item)
+    elif item.startswith("target=Name(") or item.startswith("targets=[Name("):
+        return target(item)
+
+    else:
+        return item
+
+    
+def leftName(item):
+    item = item.replace("left=Name(", "")
+    item_list = item.split(",")
+    for i in item_list:
+        if "id=" in i:
+            return i.replace("id=", "").replace("'", "")
+
+def target(item):
+    item = item.replace("target=Name(", "")
+    item_list = item.split(",")
+    for i in item_list:
+        if "id=" in i:
+            return i.replace("id=", "").replace("'", "")
+
+def valueConstant(item):
+    item = item.replace("value=Constant(", "").replace("targets=[Name(", "")
+    item_list = item.split(",")
+    for i in item_list:
+        if "value=" in i:
+            return i.replace("value=", "").replace("'", "")
+
+def value(item):
+    item = item.replace("value=Name(", "")
+    item_list = item.split(",")
+    for i in item_list:
+        if "id=" in i:
+            return i.replace("id=", "").replace("'", "")
+
+def arg(item):
+    item = item.replace("args=[arg(", "").replace("arg(", "")
+    item_list = item.split(",")
+    for i in item_list:
+        if "arg=" in i:
+            return i.replace("arg=", "").replace("'", "")
+
+
+def prettyPrint(node, all_list = []):
+    if node.children == []:
+        return
+    else:
+        for child in node.children:
+            if (node.content != "b" and child.content != "ex"):
+                all_list.append([node.content, child.content])
+        for child in node.children:
+            prettyPrint(child, all_list)
+
+def main():
+    # testcast = CreateTuple("{0}_SingleLL.py".format(1))
+    # root_Node = TreeNode("start")
+    # testcast.generate_tuples(parent = root_Node)
+    # result = []
+    # prettyPrint(root_Node, result)
+    # print(result)
+    # print(len(result))
+    # print("While" in testcast.all_tuples)
+
+
+    data = {}
+    for i in range(1, 21):
+        testcast = CreateTuple("{0}_SingleLL.py".format(i))
+        root_Node = TreeNode("start")
+        testcast.generate_tuples(parent = root_Node)
+
+        result = []
+
+        prettyPrint(root_Node, result)
+        print(len(result))
+        data[i] = result
+        # print(data)
+
+main()
